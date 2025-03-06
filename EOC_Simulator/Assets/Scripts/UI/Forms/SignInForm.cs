1e67c060eefe643f6c8a1b53ca5cc2735f67e021
Oscar Rützou <oscargamer999@gmail.com>
2025-03-06 15:53:33 -0800

    UI Changes: Side menu

---
+++ EOC_Simulator/Assets/Scripts/UI/Forms/SignInForm.cs
@@ -0,0 +1,89 @@
+using System;
+using Events;
+using PixelCrushers.DialogueSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Forms
+{
+    public class SignInForm : MonoBehaviour
+    {
+        // [SerializeField] private DialogueSystemTrigger seleteced
+        [SerializeField] private GameObject signInFormsPanel;
+        [SerializeField] private TMP_InputField nameInputField;
+        [SerializeField] private TMP_Dropdown timeInDropdown;
+        [SerializeField] private TMP_Dropdown timeOutDropdown;
+
+        [SerializeField] private Button completeButton;
+        
+        // Using a enum, to make code more readable that a bool that cant be changed
+        private enum SignState {SignIn, SignOut}
+        private SignState _signState;
+
+        private void Awake()
+        {
+            signInFormsPanel.SetActive(false);
+        }
+
+        private void Start()
+        {
+            completeButton.onClick.AddListener(OnCompleteClicked);
+        }
+
+        
+        // Make a popup that will display for some time the error.
+        private void OnCompleteClicked()
+        {
+            if (_signState.Equals(SignState.SignIn))
+            {
+                if (String.IsNullOrEmpty(nameInputField.text)) // Name not set
+                {
+                    Debug.Log($"Name not set");
+                    return;
+                }
+
+                if (timeInDropdown.value == 0)
+                {
+                    Debug.Log($"Time In not set");
+                    return;
+                }
+            }
+            else
+            {
+                if (timeOutDropdown.value == 0)
+                {
+                    Debug.Log($"Time Out not set");
+                    return;                    
+                }
+            }
+            
+            signInFormsPanel.SetActive(false);
+            InputManager.Instance.SwitchToPlayerMap();
+        }
+
+        public void OnStartSignIn()
+        {
+            InputManager.Instance.SwitchToUIMap();
+            _signState = SignState.SignIn;
+
+            nameInputField.enabled = true;
+            timeInDropdown.enabled = true;
+            timeOutDropdown.enabled = false;
+            
+            signInFormsPanel.SetActive(true);
+        }
+        
+        public void OnStartSignOut()
+        {
+            InputManager.Instance.SwitchToUIMap();
+            _signState = SignState.SignOut;
+            
+            nameInputField.enabled = false;
+            timeInDropdown.enabled = false;
+            timeOutDropdown.enabled = true;
+            
+            signInFormsPanel.SetActive(true);
+        }
+    }
+}
